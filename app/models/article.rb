e827b0822cca720f5713640f4a5faa937bd18d22
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-03 17:01:24 -0500

    Add validations for title and body of article, link to the form for creating new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  # Validate title entry
+  validates :title, presence: true
+  # Validate body with a minimum length of 10
+  validates :body, presence: true, length: { minimum: 10 }
 end
