6f3bd22e428639e9bc4bdf43c3c405bda5661082
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-04 06:16:35 -0500

    Fix routing for article comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-  resources :comments
+  resources :articles do
+    resources :comments
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
