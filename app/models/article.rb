1436564b88821fd8576007aaa46ffdd7fe3a369b
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-04 08:03:18 -0500

    Implement Concerns for statuses on articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,8 @@
 class Article < ApplicationRecord
-  # Associate comments
+  include Visible
+
   has_many :comments
 
-  # Validate title entry
   validates :title, presence: true
-  # Validate body with a minimum length of 10
   validates :body, presence: true, length: { minimum: 10 }
 end
